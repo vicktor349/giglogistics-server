57ccd72eab6ed64f6d865fe75ed907bbdbdf3c3f
vicktor349 <ayomidevictor349@gmail.com>
2023-07-01 21:59:40 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
