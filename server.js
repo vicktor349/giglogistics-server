33c8f084c505c2b11c1adc3a6bbc68ab4960b4dc
vicktor349 <ayomidevictor349@gmail.com>
2023-07-02 01:25:10 +0100

    added cors

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const connectDB = require('./config/db')
 connectDB()
 app.use(express.json())
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use('/api/auth', require('./auth/route'))
 
 
