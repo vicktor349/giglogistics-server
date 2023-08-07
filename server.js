5b1a4ca8779214e38618fa62f18c7558a106dcfd
vicktor349 <ayomidevictor349@gmail.com>
2023-08-07 11:33:23 +0100

    CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB()
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', 'https://giglogistics.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://giglogistics.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
