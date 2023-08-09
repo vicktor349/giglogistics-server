67e3bde99bdc6f569b862fb657fae1e5d192db0a
vicktor349 <ayomidevictor349@gmail.com>
2023-08-09 10:47:07 +0100

    Made changes

--- server.js
+++ server.js
@@ -8,17 +8,13 @@ app.use(express.json())
 app.use(cors())
 connectDB()
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+app.use(verifyToken, (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://giglogistics.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 app.use('/api/auth', require('./auth/route'))
-app.use(verifyToken, (req, res) => {
-    res.send('<h1>Hello</h1>')
-})
 
 
 
